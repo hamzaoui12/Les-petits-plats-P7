00b3bf6a2a6fed4fef389a81999815e7d33b9e3a
hamzaoui12 <hamzaouimerina@gmail.com>
2024-05-25 17:25:35 +0200

    chore: Ajout du linter

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  files: ["**/*.js"],
+  languageOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    "no-unused-expressions": "error",
+    "no-undefined": "error",
+    "no-unreachable": "error",
+    "no-const-assign": "error",
+    "no-constant-condition": "error",
+    "no-dupe-args": "error",
+    "no-dupe-keys": "error",
+    "no-duplicate-case": "error",
+    "no-empty": "error",
+    "no-extra-boolean-cast": "error",
+    "no-extra-semi": "error",
+    "no-func-assign": "error",
+    "no-invalid-regexp": "error",
+    "no-irregular-whitespace": "error",
+    "no-obj-calls": "error",
+    "no-sparse-arrays": "error",
+    "no-unexpected-multiline": "error",
+  },
+  ignores: ["node_modules/", ".eslintrc.js"],
+};
